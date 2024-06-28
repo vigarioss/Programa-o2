0ca0ea0b0c5929fa825ddf3c2f054c4235c76b95
EmersonSplitter <152915437+parzival070@users.noreply.github.com>
2024-06-28 08:41:23 -0300

    parte2

--- App.js
+++ App.js
@@ -18,3 +18,61 @@ function handleDBError(res, err) {
     console.error(err);
     res.status(500).json({ error: 'Erro no servidor' });
 }
+
+app.get('/EntidadeA', async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('select * from EntidadeA');
+        connection.release();
+        res.json(rows);
+    } catch (err) {
+        handleDBError(res, err);
+    }
+});
+
+app.get('/EntidadeB', async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('select * from EntidadeB');
+        connection.release();
+        res.json(rows);
+    } catch (err) {
+        handleDBError(res, err);
+    }
+});
+
+app.get('/EntidadeA/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('select * from EntidadeA where id = ?', [id]);
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'Registro não encontrado' });
+            return;
+        }
+        const [rowsB] = await connection.query('select * from EntidadeB where EntidadeA_id = ?', [id]);
+        connection.release();
+        const result = { ...rows[0], EntidadeB: rowsB };
+        res.json(result);
+    } catch (err) {
+        handleDBError(res, err);
+    }
+});
+
+app.get('/EntidadeB/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.query('select * from EntidadeB where id = ?', [id]);
+        if (rows.length === 0) {
+            res.status(404).json({ error: 'Registro não encontrado' });
+            return;
+        }
+        const [rowsA] = await connection.query('select * from EntidadeA where id = ?', [rows[0].EntidadeA_id]);
+        connection.release();
+        const result = { ...rows[0], EntidadeA: rowsA[0] };
+        res.json(result);
+    } catch (err) {
+        handleDBError(res, err);
+    }
+});
